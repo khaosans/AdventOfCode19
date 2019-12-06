4d894a66fe7ac114a170f622c29d1659fa7a7e95
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2019-12-05 17:37:42 -0800

    Day 4

--- Day4/Day4.cs
+++ Day4/Day4.cs
@@ -16,16 +16,32 @@ namespace Day4V1
             {
                 list.Add(i);
             }
-            
-            return list.Select(x => x.ToString()).Where(HasDouble)
+
+            return list.Select(x => x.ToString()).Where(HasDoubleOrMore)
+                .Where(Increasing)
+                .ToList().Count.ToString();
+        }
+
+        public static string isValid2(string range)
+        {
+            var (min, max) = ParseRange("245318-765747");
+
+
+            List<Int32> list = new List<int>();
+            for (var i = min; i < max; i++)
+            {
+                list.Add(i);
+            }
+
+            return list.Select(x => x.ToString()).Where(HasDoubleExactly)
                 .Where(Increasing)
                 .ToList().Count.ToString();
         }
 
 
-        public static bool HasDouble(string intput)
+        public static bool HasDoubleOrMore(string intput)
         {
-            for (var i = 0; i < intput.Length -1; ++i)
+            for (var i = 0; i < intput.Length - 1; ++i)
             {
                 if (intput[i].Equals(intput[i + 1]))
                 {
@@ -36,9 +52,33 @@ namespace Day4V1
             return false;
         }
 
+        public static bool HasDoubleExactly(string input)
+        {
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+
+            foreach (var num in Enumerable.Range(0, 10))
+            {
+                counter[num.ToString()] = 0;
+            }
+
+            for (var i = 0; i < input.Length - 1; ++i)
+            {
+                if (i < 4 && input[i].Equals( input[i + 1]))
+                {
+                    counter[input[i].ToString()]++;
+                }
+            }
+
+            if ( input[4].Equals( input[5]))
+            {
+                counter[input[5].ToString()]++;
+            }
+            return counter.Values.Any(x => x.Equals(1));
+        }
+
         public static bool Increasing(string input)
         {
-            for (var i = 0; i < input.Length -1; ++i)
+            for (var i = 0; i < input.Length - 1; ++i)
             {
                 if (input[i] > input[i + 1])
                 {
