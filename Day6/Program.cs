e197ed5de1923574354676521c9635a8f8768ccc
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2019-12-15 21:23:37 -0800

    Day 6 part 1

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -8,16 +8,66 @@ namespace Day6
     {
         static void Main(string[] args)
         {
-            List<Orbit> orbits = "/Users/souriyakhaosanga/Documents/AdventOfCode/Day6/day6Sample.txt".ParseOrbits();
+            List<Orbit> orbits = "/Users/souriyakhaosanga/Documents/AdventOfCode/Day6/day6.txt".ParseOrbits();
 
-            var dictionary = CreateOrbitalMap(orbits);
+         //   var dictionary = CreateOrbitalMap(orbits);
 
-            var keyValuePairs = dictionary.ToDictionary(x => x.Value, x => x.Key);
+         Dictionary<string,string> orbitalParent = CreateOrbitalParent(orbits);
+
+         int shouldBe3 = GetPaths(orbitalParent, "D");
+
+         int ShouldBe7 = GetPaths(orbitalParent, "L");
+
+         int paths = GetPaths(orbitalParent, "COM");
+
+         int count = 0;
+
+         foreach (var orbit in orbits)
+         {
+             count += GetPaths(orbitalParent,orbit.Child);
+             
+         }
+         
+         
 
 
-            int count = keyValuePairs.Keys.Count(x => x.Contains("D"));
 
+         Console.Out.WriteLine("count " + count);
         }
+
+        public static int GetPaths(Dictionary<string, string> dictionary, string Sattelite)
+        {
+            if (!dictionary.ContainsKey(Sattelite) )
+            {
+                return 0;
+            }
+            
+            var count = 1;
+            while (dictionary[Sattelite] != "COM")
+            {
+                count++;
+                Sattelite = dictionary[Sattelite];
+            }
+
+            return count;
+        }
+
+        public static int GetCount(Dictionary<string, List<string>> dictionary, List<Orbit> orbits, int counter)
+        {
+            if (orbits.Count == 0)
+            {
+                return counter;
+            }
+
+            if (dictionary.ContainsKey(orbits.First().Child))
+            {
+                List<string> children = dictionary[orbits.First().Child];
+                return counter + GetCount(dictionary, orbits.Skip(1).ToList(), counter + children.Count);
+            }
+
+            return GetCount(dictionary, orbits.Skip(1).ToList(), counter);
+        }
+
 
         private static Dictionary<string, List<string>> CreateOrbitalMap(List<Orbit> orbits)
         {
@@ -25,13 +75,28 @@ namespace Day6
 
             foreach (var orbit in orbits)
             {
-                if (!dictionary.ContainsKey(orbit.CenterOfMass))
+                if (!dictionary.ContainsKey(orbit.Parent))
                 {
-                    dictionary[orbit.CenterOfMass] = new List<string> {orbit.Satellite};
+                    dictionary[orbit.Parent] = new List<string> {orbit.Child};
                 }
                 else
                 {
-                    dictionary[orbit.CenterOfMass].Add(orbit.Satellite);
+                    dictionary[orbit.Parent].Add(orbit.Child);
+                }
+            }
+
+            return dictionary;
+        }
+
+        private static Dictionary<string, string> CreateOrbitalParent(List<Orbit> orbits)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            foreach (var orbit in orbits)
+            {
+                if (orbit.Child != null)
+                {
+                    dictionary[orbit.Child] = orbit.Parent;
                 }
             }
 
@@ -42,18 +107,18 @@ namespace Day6
 
     public class Orbit
     {
-        public string CenterOfMass { get; }
-        public string Satellite { get; }
+        public string Parent { get; }
+        public string Child { get; }
 
-        public Orbit(string centerOfMass, string satellite)
+        public Orbit(string parent, string child)
         {
-            CenterOfMass = centerOfMass;
-            Satellite = satellite;
+            Parent = parent;
+            Child = child;
         }
 
         public override string ToString()
         {
-            return $"{Satellite} orbits {CenterOfMass}";
+            return $"{Child} orbits {Parent}";
         }
     }
 }
