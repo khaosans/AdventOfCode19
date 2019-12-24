bbfe420b564cb859c3ffaebcbdce11eba66f39cb
Souriya Khaosanga <souriya.khaosanga@gmail.com>
2019-12-23 19:15:31 -0800

    day 8 complete

--- Day8/Picture.cs
+++ Day8/Picture.cs
@@ -1,42 +1,133 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace Day8
 {
     public class Picture
     {
-        public Dictionary<int, List<int>> Layers = new Dictionary<int, List<int>>();
         public int Height;
         public int Length;
         public List<int> Input;
+        public List<int[,]> layers = new List<int[,]>();
         public int LayerSize => Height * Length;
 
 
-        public Picture(List<int> input, int height, int length)
+        public Picture(List<int> input, int length, int height)
         {
             Height = height;
             Length = length;
-            Input = input;
+            Input = new List<int>(input);
         }
 
 
-        public Dictionary<int, List<int>> GetLayers()
+        public List<int[,]> GetLayers()
         {
             var counter = 0;
+
             while (counter < Input.Count)
             {
-              
                 List<int> list = Input.Take(LayerSize).ToList();
-                Layers.Add(counter, list);
+                var ints = initialize();
+                int index = 0;
+                for (int i = 0; i < Length; i++)
+                {
+                    for (int j = 0; j < Height; j++)
+                    {
+                        ints[i, j] = list[index];
+                        index++;
+                    }
+                }
+
+                layers.Add(ints);
 
                 Input = Input.Skip(LayerSize).ToList();
-                
+
                 counter++;
             }
 
-            return Layers;
+            return layers;
         }
+
+        public int[,] initialize()
+        {
+            int[,] output = new int[Length, Height];
+
+
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Height; j++)
+                {
+                    output[i, j] = -1;
+                }
+            }
+
+            return output;
+        }
+
+        public void Decode()
+        {
+            Dictionary<int, int> pixel = new Dictionary<int, int>();
+
+            var display = CreteCreateLayer();
+
+            Console.Out.WriteLine($"{display}");
+
+            int len = 0;
+            for (int i = 0; i < Length; i++)
+            {
+
+                for (int j = 0; j < Height; j++)
+                {
+                    len++;
+                    var output = display[i,j];
+
+                    if (output == 0)
+                    {
+                        Console.Out.Write($" ");
+
+                    }
+                    else
+                    {
+                        Console.Out.Write("X");
+                    }
+                    if (len % Length == 0)
+                    {
+                        Console.Out.WriteLine("");
+                    }
+                    
+                }
+            }
+            
+
+        }
+
+       
+
+        private int[,] CreteCreateLayer()
+        {
+            var display = initialize();
+
+            foreach (var array in layers)
+            {
+                for (int i = 0; i < Length; i++)
+                {
+                    for (int j = 0; j < Height; j++)
+                    {
+                        var i1 = array[i, j];
+
+                        if (IsConcrete(i1) && display[i, j] == -1)
+                        {
+                            display[i, j] = i1;
+                        }
+                        
+                    }
+                }
+            }
+
+            return display;
+        }
+
+        private Predicate<int> IsConcrete = color => color == 0 || color == 1;
     }
 }
